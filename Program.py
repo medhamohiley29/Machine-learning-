13af74b1154b7c8d41259e03514eff3aeee67c66
medhamohiley29 <medhamohiley@gmail.com>
2026-01-16 10:51:55 +0530

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Original array:", arr)
+
+index = 2      
+new_value = 99 
+arr[index] = new_value
+
+print("Modified array:", arr)
